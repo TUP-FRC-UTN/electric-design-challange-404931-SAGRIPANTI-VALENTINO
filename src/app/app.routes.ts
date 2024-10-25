85faf6ce9fbb03f2311983e67658e36ca5e7fb02
Val <404931@tecnicatura.frc.utn.edu.ar>
2024-10-25 14:41:35 -0300

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { BudgetListComponent } from './budget-list/budget-list.component';
+import { BudgetFormComponent } from './budget-form/budget-form.component';
+import { BudgetViewComponent } from './budget-view/budget-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "list", component : BudgetListComponent
+    },
+    {
+        path: "form", component : BudgetFormComponent
+    },
+    {
+        path: "view", component : BudgetViewComponent
+    }
+];
